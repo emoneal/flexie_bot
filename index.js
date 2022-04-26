b3d2b4cfebfc3a2ced7aeeb132bcd2aad96ed73a
Emi ONeal <atashiemichandesu@gmail.com>
2022-04-26 23:15:22 +0900

    cleaned up code, integrated code into one file for ease of use

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv');
 const tmi = require('tmi.js');
+const { Client: twitch } = require('tmi.js')
 
 
 dotenv.config();
@@ -32,3 +33,64 @@ client.on('interactionCreate', async interaction => {
 
 // Login to Discord with your client's token
 client.login(process.env.TOKEN);
+
+
+// Twitch Bot
+
+// Define configuration options
+const opts = {
+	identity: {
+	  username: process.env.BOT_USERNAME,
+	  password: process.env.OAUTH_TOKEN,
+	},
+	channels: [process.env.CHANNEL_NAME],
+  };
+  
+  // Create a client with our options
+  const twitchBot = new tmi.client(opts);
+  
+  // Register our event handlers (defined below)
+  twitchBot.on("message", onMessageHandler);
+  twitchBot.on("connected", onConnectedHandler);
+  
+  // Connect to Twitch:
+  twitchBot.connect();
+  
+  // Called every time a message comes in
+  function onMessageHandler(target, context, msg, self) {
+	if (self) {
+	  return;
+	} // Ignore messages from the bot
+  
+	// Remove whitespace from chat message
+	const commandName = msg.trim();
+  
+	// If the command is known, let's execute it
+	if (commandName === "!dice") {
+	  const num = rollDice(commandName);
+	  twitchBot.say(
+		target,
+		`You rolled a 12-sided dice. It's a ${num}!`
+	  );
+  
+	  if (num === 7) {
+		  
+	  }
+  
+	  console.log(`* Executed ${commandName} command`);
+	} else {
+	  console.log(`* Unknown command ${commandName}`);
+	}
+  }
+  
+  // Function called when the "dice" command is issued
+  function rollDice() {
+	const sides = 12;
+	return Math.floor(Math.random() * sides) + 1;
+  }
+  
+  // Called every time the bot connects to Twitch chat
+  function onConnectedHandler(addr, port) {
+	console.log(`* Connected to ${addr}:${port}`);
+	
+  }
